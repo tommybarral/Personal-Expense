cbad19db31affc9d995703faf68dbe3874107527
Tommybarral <isamutom27@hotmail.com>
2022-02-10 14:43:56 +0100

    adding and cleaning all the project

---
+++ lib/screens/MyHomePage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Personal expense for GIT'),),
+      body: Center(child: Text('Hello and welcome to this expense project.'),),
+    );
+  }
+}
