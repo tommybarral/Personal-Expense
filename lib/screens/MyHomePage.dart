e59d3332fe4040d000336d2980c5460e3f57a963
Tommybarral <isamutom27@hotmail.com>
2022-02-11 13:44:07 +0100

    implementation of intl package

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../model/transaction.dart';
+import 'package:intl/intl.dart';
 
 
 class MyHomePage extends StatelessWidget {
@@ -47,14 +48,14 @@ class MyHomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(tx.title, style: TextStyle( fontSize: 20, fontWeight: FontWeight.bold),),
-                        Text(tx.date.toString(), style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900, color: Colors.grey),),
+                        Text(DateFormat.yMMMMEEEEd().format(tx.date), style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900, color: Colors.grey),),
                       ],
                     ),
                     Container(
                       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2),),
-                      child: Text(tx.amount.toString() + ' €', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.purple),),
+                      child: Text('${tx.amount.toString()} €', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.purple),),
                     ),
                   ],
                 ),
